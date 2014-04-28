501202f4a71c32f8537fb06e276921366525ff9b
marian <marian@marian-pc.(none)>
2014-04-28 15:16:25 +0300

    Manapool update

--- manapool.c
+++ manapool.c
@@ -3,16 +3,34 @@
 
 int can_put_card(card_t card_to_put, manapool_t pool){
 	if(card_to_put.card_mana > pool.current_mana){
-		printf("0\n");
 		return 0;
 	} else {
 		return 1;
 	}
 }
 
+void add_mana (manapool_t *pool, int mana_to_add){
+	pool->current_mana += mana_to_add;
+}
+
+void remove_mana (manapool_t *pool, int mana_to_remove){
+	pool->current_mana -= mana_to_remove;
+}
+
 int main(){
 	card_t attacker = {"Almighty PATKA", 4123, 12, 12};
 	manapool_t pool = {1, 1};
+
 	can_put_card(attacker, pool);
+
+	/*
+		TESTING add & remove mana
+
+		add_mana(&pool, 5);
+		remove_mana(&pool, 2);
+
+		printf("%d\n", pool.current_mana);
+
+	*/
 	return 0;
 }
--- manapool.h
+++ manapool.h
@@ -8,6 +8,8 @@
 		int current_mana;
 	} manapool_t;
 
-	int can_put_card(card_t card_to_put, manapool_t pool);
+	int can_put_card (card_t card_to_put, manapool_t pool);
+	void add_mana (manapool_t *pool, int mana_to_add);
+	void remove_mana (manapool_t *pool, int mana_to_remove);
 
 #endif
