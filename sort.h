aefa832f06fb371f4b56f94edb4f5532d4403215
mbelchev <marianbelchev@gmail.com>
2014-06-05 13:51:21 +0300

    Upadate sort

---
+++ sort.h
@@ -0,0 +1,12 @@
+#ifndef SORT_H
+    #define SORT_H
+    #include "player.h"
+
+    void swap(card_t *card1, card_t *card2);
+    void sort(player_t *player, int sort_by);
+    
+//    SORT BY: CASE 1: by dmg
+//             CASE 2: by hp
+//             CASE 3: by mana
+    
+#endif
